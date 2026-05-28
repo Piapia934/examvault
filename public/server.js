b705a2469d7f5d2325a43d5b4d8598a96eb76153
unknown <piyacutie13@gmail.com>
2026-05-28 20:23:00 +0800

    Fixed PWA public folder

--- public/server.js
+++ public/server.js
@@ -2,18 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-// Serve files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Open index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Fallback route
-app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
